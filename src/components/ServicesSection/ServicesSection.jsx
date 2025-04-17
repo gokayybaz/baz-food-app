3a166dba5e532650b0a23d4a54634353107216b7
gokayybaz <gokaybaz2000@outlook.com>
2025-04-17 06:11:14 +0300

    [Completed] - Services Section Mobile Design Completed.

--- src/components/ServicesSection/ServicesSection.jsx
+++ src/components/ServicesSection/ServicesSection.jsx
@@ -5,7 +5,7 @@ import ServicesSectionCardArea from './ServicesSectionCardArea';
 
 const ServicesSection = () => {
     return (
-        <section className='mt-25 px-36'>
+        <section className='m-4 md:m-0 mt-20 md:mt-25   md:px-36'>
             <ServicesSectionTitleArea />
             <ServicesSectionCardArea />
         </section>
--- src/components/ServicesSection/ServicesSectionCardArea.jsx
+++ src/components/ServicesSection/ServicesSectionCardArea.jsx
@@ -10,7 +10,7 @@ import ServicesSectionCard from './ServicesSectionCard/ServicesSectionCard'
 
 const ServicesSectionCardArea = () => {
     return (
-        <div className='grid grid-cols-3 mt-10 gap-8'>
+        <div className='grid grid-cols-1 md:grid-cols-3 mt-10 gap-8'>
             <ServicesSectionCard Icon={SiFresh} title={"TAZE ve DOĞAL MALZEME"} description={"Özenle seçilmiş doğal ve taze malzemelerle gerçek lezzeti sofralarınıza taşıyoruz."} />
             <ServicesSectionCard Icon={MdCleanHands} title={"TEMİZ ve GÜVENİLİR"} description={"En hijyenik koşullarda, güvenilir lezzetleri sofralarınıza sunuyoruz."} />
             <ServicesSectionCard Icon={TbTruckDelivery} title={"HIZLI TESLİMAT"} description={"Siparişiniz en kısa sürede, güvenli ve hızlı bir şekilde kapınızda."} />
